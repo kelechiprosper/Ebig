2cbd1bbb644643df759560a8ffd808e07bf4d7b2
kelechi prosper <prosperkelechi50@gmail.com>
2021-11-18 12:32:30 +0100

    i

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus")
 
 function jump() {
     if (dino.classList != "jump"){
@@ -9,6 +10,18 @@ function jump() {
     }, 300);
 }
 }
+
+let isAlive = setInterval(function () {
+    let dinoTop = parseInt (window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    
+
+    if(cactusleft <50 && cactusleft > 0 && dinoTop >= 140)
+    alert("game over!");
+
+
+
+},10);
 
 document.addEventListener("keydown", function (event) {
     jump();
